07e948a87353e705e6a78e2640b1be677052f785
minhuizhong <minhui.zhong2015@gmail.com>
2025-02-11 14:01:43 +0100

    Ny update

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -21,13 +21,13 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Invoke("SpawnArmchair");
+       
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+       
     }
 
     public void SpawnArmchair()
