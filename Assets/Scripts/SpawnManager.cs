268faaadd46317c3e780adec621f62ba511a3164
minhuizhong <minhui.zhong2015@gmail.com>
2025-02-13 16:01:31 +0100

    New UI

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,32 +6,37 @@ using Vector3 = UnityEngine.Vector3;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject armchairPrefab;
+    public GameObject[] furniture;
+    //public GameObject armchairPrefab;
 
-    public GameObject bookcasePrefab;
+    //public GameObject bookcasePrefab;
 
-    public GameObject coffeeTablePrefab;
+    //public GameObject coffeeTablePrefab;
 
-    public GameObject plant1Prefab;
+    //public GameObject plant1Prefab;
 
-    public GameObject plant2Prefab;
+    //public GameObject plant2Prefab;
 
-    private static readonly Vector3 SpawnPosition = new (0,0,0);
-    
+    private static readonly Vector3 SpawnPosition = new(0, 0, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-       
     }
 
     // Update is called once per frame
     void Update()
-    { 
-       
+    {
     }
 
-    public void SpawnArmchair()
+
+    public void SpawnFurniture(int furnitureIndex)
     {
-        Instantiate(armchairPrefab, SpawnPosition, armchairPrefab.transform.rotation);
+        //Instantiate(armchairPrefab, SpawnPosition, armchairPrefab.transform.rotation);
+        //Instantiate(bookcasePrefab, SpawnPosition, bookcasePrefab.transform.rotation);
+        //Instantiate(coffeeTablePrefab, SpawnPosition, coffeeTablePrefab.transform.rotation);
+        //Instantiate(plant1Prefab, SpawnPosition, plant1Prefab.transform.rotation);
+        //Instantiate(plant2Prefab, SpawnPosition, plant2Prefab.transform.rotation);
+        Instantiate(furniture[furnitureIndex], SpawnPosition, furniture[furnitureIndex].transform.rotation);
     }
 }
